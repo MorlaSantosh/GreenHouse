e9b5687671a9f143ef791a5a93d0f11a9181da2c
Prateek lodhi <mailprateeklodhi@gmail.com>
2021-08-03 00:57:17 +0530

    commit

--- ToDoProject/settings.py
+++ ToDoProject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+DISABLE_COLLECTSTATIC=1
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
